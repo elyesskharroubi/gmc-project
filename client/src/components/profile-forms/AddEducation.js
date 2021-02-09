2fdd437ea6c54a6f3b159a980b5a592243dbca7e
elyesskharroubi <medelyesskharroubi@outlook.com>
2021-02-09 16:04:43 +0100

    update

--- client/src/components/profile-forms/AddEducation.js
+++ client/src/components/profile-forms/AddEducation.js
@@ -1,33 +1,34 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { Link, useHistory } from "react-router-dom";
-import { addEducation, addExperience } from "../../actions/profile";
+import { addEducation } from "../../actions/profile";
 
 import "../auth/Auth.css";
 import "../dashboard/Dashboard.css";
 
 const AddEducation = () => {
-    const history = useHistory()
-    const dispatch = useDispatch()
-    const [formData, setFormData] = useState({
-        school:'',
-        degree:'',
-        from:'',
-        current: false,
-        to: '',
-        description: ''
-    })
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const [formData, setFormData] = useState({
+    school: "",
+    degree: "",
+    from: "",
+    current: false,
+    to: "",
+    description: "",
+  });
 
-    const [toDateDisabled, toggleDisabled] = useState(false)
-    const {school, degree, from, current, to, description} = formData
-    const onChange = e => setFormData({...formData, [e.target.name]: e.target.value})
-    const onSubmit = e => {
-        e.preventDefault()
-        dispatch(addEducation(formData, history))
-    }
+  const [toDateDisabled, toggleDisabled] = useState(false);
+  const { school, degree, from, current, to, description } = formData;
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  const onSubmit = (e) => {
+    e.preventDefault();
+    dispatch(addEducation(formData, history));
+  };
   return (
     <section className="container">
-      <form className="profileForm" onSubmit={e => onSubmit(e)}>
+      <form className="profileForm" onSubmit={(e) => onSubmit(e)}>
         <h1 style={{ fontSize: "30px" }}>Add an Experience</h1>
         <p className="text">
           Add any design / marketing related experience that you have had in the
@@ -40,33 +41,66 @@ const AddEducation = () => {
           <small className="smallDesc">
             <em>School or Bootcamp</em>
           </small>
-          <input type="text" placeholder="School *" name="school" value={school} onChange={e=> onChange(e)} required />
+          <input
+            type="text"
+            placeholder="School *"
+            name="school"
+            value={school}
+            onChange={(e) => onChange(e)}
+            required
+          />
         </div>
         <div className="create-profile-input">
           <small className="smallDesc">
             <em>Name of the Company</em>
           </small>
-          <input type="text" placeholder="Degree *" name="degree" value={degree} onChange={e=> onChange(e)} required />
+          <input
+            type="text"
+            placeholder="Degree *"
+            name="degree"
+            value={degree}
+            onChange={(e) => onChange(e)}
+            required
+          />
         </div>
         <div className="create-profile-input">
           <small className="smallDesc">
             <em>From date *</em>
           </small>
-          <input type="date" name="from" value={from} onChange={e=> onChange(e)} required />
+          <input
+            type="date"
+            name="from"
+            value={from}
+            onChange={(e) => onChange(e)}
+            required
+          />
         </div>
         <div className="create-profile-input">
           <p className="text">
-            <input type="checkbox" name="current" checked={current} value={current} onChange={e =>{
-                setFormData({...formData, current:!current});
-                toggleDisabled(!toDateDisabled)
-            }} /> Current Education
+            <input
+              type="checkbox"
+              name="current"
+              checked={current}
+              value={current}
+              onChange={(e) => {
+                setFormData({ ...formData, current: !current });
+                toggleDisabled(!toDateDisabled);
+              }}
+            />{" "}
+            Current Education
           </p>
         </div>
         <div className="create-profile-input">
           <small className="smallDesc">
             <em>To date</em>
           </small>
-          <input type="date" name="to" value={to} onChange={e=> onChange(e)} disabled={toDateDisabled ? 'disabled' : ''} />
+          <input
+            type="date"
+            name="to"
+            value={to}
+            onChange={(e) => onChange(e)}
+            disabled={toDateDisabled ? "disabled" : ""}
+          />
         </div>
         <div className="create-profile-input">
           <small className="smallDesc">
@@ -77,7 +111,8 @@ const AddEducation = () => {
             placeholder="Description"
             cols="30"
             rows="5"
-            value={description} onChange={e=> onChange(e)}
+            value={description}
+            onChange={(e) => onChange(e)}
           ></textarea>
         </div>
 
--- client/src/components/profile-forms/AddExperience.js
+++ client/src/components/profile-forms/AddExperience.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { Link, useHistory } from "react-router-dom";
 import { addExperience } from "../../actions/profile";
@@ -7,28 +7,29 @@ import "../auth/Auth.css";
 import "../dashboard/Dashboard.css";
 
 const AddExperience = () => {
-    const history = useHistory()
-    const dispatch = useDispatch()
-    const [formData, setFormData] = useState({
-        title:'',
-        company:'',
-        location:'',
-        from:'',
-        current: false,
-        to: '',
-        description: ''
-    })
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const [formData, setFormData] = useState({
+    title: "",
+    company: "",
+    location: "",
+    from: "",
+    current: false,
+    to: "",
+    description: "",
+  });
 
-    const [toDateDisabled, toggleDisabled] = useState(false)
-    const {title, company, location, from, current, to, description} = formData
-    const onChange = e => setFormData({...formData, [e.target.name]: e.target.value})
-    const onSubmit = e => {
-        e.preventDefault()
-        dispatch(addExperience(formData, history))
-    }
+  const [toDateDisabled, toggleDisabled] = useState(false);
+  const { title, company, location, from, current, to, description } = formData;
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  const onSubmit = (e) => {
+    e.preventDefault();
+    dispatch(addExperience(formData, history));
+  };
   return (
     <section className="container">
-      <form className="profileForm" onSubmit={e => onSubmit(e)}>
+      <form className="profileForm" onSubmit={(e) => onSubmit(e)}>
         <h1 style={{ fontSize: "30px" }}>Add an Experience</h1>
         <p className="text">
           Add any design / marketing related experience that you have had in the
@@ -41,39 +42,78 @@ const AddExperience = () => {
           <small className="smallDesc">
             <em>Job title (position)</em>
           </small>
-          <input type="text" placeholder="Title *" name="title" value={title} onChange={e=> onChange(e)} required />
+          <input
+            type="text"
+            placeholder="Title *"
+            name="title"
+            value={title}
+            onChange={(e) => onChange(e)}
+            required
+          />
         </div>
         <div className="create-profile-input">
           <small className="smallDesc">
             <em>Name of the Company</em>
           </small>
-          <input type="text" placeholder="Company *" name="company" value={company} onChange={e=> onChange(e)} required />
+          <input
+            type="text"
+            placeholder="Company *"
+            name="company"
+            value={company}
+            onChange={(e) => onChange(e)}
+            required
+          />
         </div>
         <div className="create-profile-input">
           <small className="smallDesc">
             <em>Where did it took place</em>
           </small>
-          <input type="text" placeholder="Location *" name="location" value={location} onChange={e=> onChange(e)} />
+          <input
+            type="text"
+            placeholder="Location *"
+            name="location"
+            value={location}
+            onChange={(e) => onChange(e)}
+          />
         </div>
         <div className="create-profile-input">
           <small className="smallDesc">
             <em>From date *</em>
           </small>
-          <input type="date" name="from" value={from} onChange={e=> onChange(e)} required />
+          <input
+            type="date"
+            name="from"
+            value={from}
+            onChange={(e) => onChange(e)}
+            required
+          />
         </div>
         <div className="create-profile-input">
           <p className="text">
-            <input type="checkbox" name="current" checked={current} value={current} onChange={e =>{
-                setFormData({...formData, current:!current});
-                toggleDisabled(!toDateDisabled)
-            }} /> Current Job
+            <input
+              type="checkbox"
+              name="current"
+              checked={current}
+              value={current}
+              onChange={(e) => {
+                setFormData({ ...formData, current: !current });
+                toggleDisabled(!toDateDisabled);
+              }}
+            />{" "}
+            Current Job
           </p>
         </div>
         <div className="create-profile-input">
           <small className="smallDesc">
             <em>To date</em>
           </small>
-          <input type="date" name="to" value={to} onChange={e=> onChange(e)} disabled={toDateDisabled ? 'disabled' : ''} />
+          <input
+            type="date"
+            name="to"
+            value={to}
+            onChange={(e) => onChange(e)}
+            disabled={toDateDisabled ? "disabled" : ""}
+          />
         </div>
         <div className="create-profile-input">
           <small className="smallDesc">
@@ -84,7 +124,8 @@ const AddExperience = () => {
             placeholder="Description"
             cols="30"
             rows="5"
-            value={description} onChange={e=> onChange(e)}
+            value={description}
+            onChange={(e) => onChange(e)}
           ></textarea>
         </div>
 
