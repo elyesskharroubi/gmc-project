3ee3b68727134d6410d1032ce2ab440f00651321
elyesskharroubi <medelyesskharroubi@outlook.com>
2021-02-28 12:59:20 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,28 @@
-const express = require('express')
-const app = express()
-const PORT = process.env.PORT || 5000
+const express = require("express");
+const connectDB = require("./config/db");
+const path = require("path");
 
-app.get('/', (req, res) => res.send('API Running'))
+const app = express();
 
-app.listen(PORT, () => console.log(`Server started on ${PORT}`))
+// connect database
+connectDB();
+
+// init middleware
+app.use(express.json({ extended: false }));
+
+// define routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
+
+if (process.env.NODE_END === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on ${PORT}`));
